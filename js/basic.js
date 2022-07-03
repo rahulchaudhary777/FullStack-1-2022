7e31502556998a8bcbb7a5fa2ff7aa5a3610fce5
rahulchaudhary777 <ch.rahuljaat777@gmail.com>
2022-07-03 21:31:52 +0530

    Creation if non-responsive navbar

---
+++ js/basic.js
@@ -0,0 +1,47 @@
+var a = prompt("Enter First Value");
+var b = prompt("Enter Second Value");
+
+// addition 
+var add = +a+ +b;
+
+// Subtraction
+var sub = +a - +b;
+
+// Multiply
+var mul = +a * +b;
+
+// Divide
+var div = +a / +b;
+
+// Display
+console.log(
+  "Addition :- " +
+    add +
+    "\nSubtraction :-" +
+    sub +
+    "\nMultiplication :- " +
+    mul +
+    "\nDivide :-" +
+    div
+);
+document.write(
+  "Addition :- " +
+    add +
+    "\nSubtraction :-" +
+    sub +
+    "\nMultiplication :- " +
+    mul +
+    "\nDivide :-" +
+    div
+);
+alert(
+  "Addition :- " +
+    add +
+    "\nSubtraction :-" +
+    sub +
+    "\nMultiplication :- " +
+    mul +
+    "\nDivide :-" +
+    div
+);
+
